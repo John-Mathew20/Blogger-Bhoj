8b3262aabbb90228f61b3f51192dd82ab44ed508
John Mathew14 <JohnMathew141105@gmail.com>
2026-04-14 11:13:07 +0530

    added comments and files were modified

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const { BlockList } = require("net");
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "/public")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
